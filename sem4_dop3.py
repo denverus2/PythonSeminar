8b4047eecaaf6fb8b5916a88691612f8d3f0d3f2
Denverus <61493273+denverus2@users.noreply.github.com>
2023-08-30 13:35:21 +0300

    add superhard HW

---
+++ sem4_dop3.py
@@ -0,0 +1,74 @@
+# Даны два многочлена, которые вводит пользователь. как две строки.
+# Задача - сформировать многочлен, содержащий сумму многочленов, и вывести как строку.
+
+# Степени многочленов могут быть разные.
+
+# например на входе 2x^2 + 4x + 5 = 0 и 5x^3 - 3*x^2 - 12 = 0
+# на выходе будет 5x^3 - x^2 + 4х - 7 = 0
+
+
+def multi_dick_summ(lib1, lib2):
+    lib_out = {}
+    maximum = max(max(list(lib1.keys())), max(list(lib2.keys())))
+    for i in reversed(range(maximum + 1)):
+        if i in lib1 and i in lib2:
+            lib_out[i] = lib1[i] + lib2[i]
+        elif i in lib1:
+            lib_out[i] = lib1[i]
+        elif i in lib2:
+            lib_out[i] = lib2[i]
+
+    return lib_out
+
+
+def multi_dick_to_lib(str1):
+    str1 = str1.replace(" = 0", "")
+    str1 = str1.replace("+ ", "+")
+    str1 = str1.replace("- ", "-")
+    str1 = str1.replace("*", "")
+    sp1 = str1.split()
+    lib = {}
+    for i in sp1:
+        if ("^" not in i) and "x" not in i:
+            lib[0] = int(i.replace("+", ""))
+        elif ("^" not in i) and "x" in i:
+            lib[1] = int(i.replace("+", "").replace("x", ""))
+        else:
+            lib[int(i.replace("+", "").split("^")[1])] = int(
+                i.replace("+", "").split("^")[0].replace("x", "")
+            )
+    return lib
+
+
+def multi_dick_to_str(lib):
+  string = ""
+  for i in lib:
+    if i>0:
+      if lib[i]>0:
+          string=string+f' + {str(abs(lib[i]))}x^{str(i)}'
+      if lib[i]==1:
+          string=string+f' + {str(abs(lib[i]))}x'
+      elif lib[i]==0:
+          string=string+f''
+      if lib[i]<0:
+          string=string+f' - {str(abs(lib[i]))}x^{str(i)}'
+    else:
+      if lib[i]>0:
+        string=string+f' + {str(abs(lib[i]))}'
+      else:
+        string=string+f' - {str(abs(lib[i]))}'
+        
+        
+  if string.startswith(" + "):
+    string = string[3:]
+  string= string.replace("1x","x")
+  return string + " = 0"
+
+
+str1 = "2x^2 + 4x + 5 = 0"
+str2 = "5x^3 - 3*x^2 - 12 = 0"
+lib1 = multi_dick_to_lib(str1)
+lib2 = multi_dick_to_lib(str2)
+
+lib_final = multi_dick_summ(lib1, lib2)
+print(multi_dick_to_str(lib_final))
--- task21.py
+++ task21.py
@@ -9,7 +9,7 @@
 sp=[{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII ":" S007 "}]
 sp2=[]
 for i in sp:
-  x=list(i.values())
+  x=list(lib[i]s())
   sp2+= x
 for i in range(len(sp2)):
   sp2[i]=sp2[i].strip()
