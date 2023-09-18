b558250eff4a585fade8bc8f53d1633f17f56eb2
Denverus <denverus@gmail.com>
2023-09-18 19:39:35 +0300

    telegramm bot

--- telebot.py
+++ telebot.py
@@ -1,52 +1,83 @@
-import telegram
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-import google.auth
-from googleapiclient.discovery import build
+
+import sqlite3
+from telegram import Update,InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes,InlineQueryHandler,MessageHandler, filters
+
+conn = sqlite3.connect('films.db', check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS films
+                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   title TEXT,
+                   year INTEGER,
+                   director TEXT,
+                   description TEXT)''')
+conn.commit()
+
+async def add_film(update, context):
+    title = context.args[0]
+    year = context.args[1]
+    director = context.args[2]
+    description = ' '.join(context.args[3:])
+    cursor.execute("INSERT INTO films (title, year, director, description) VALUES (?, ?, ?, ?)",
+                   (title, year, director, description))
+    conn.commit()
+    await update.message.reply_text('Фильм успешно добавлен в вашу фильмотеку!')
+
+# def delete_film(update, context):
+#     chat_id = update.message.chat_id
+#     title = ' '.join(context.args)
+#     cursor.execute("DELETE FROM films WHERE title=?", (title,))
+#     conn.commit()
+#     update.message.reply_text('Фильм успешно удален из вашей фильмотеки!')
 
 
-TOKEN = '556541562:AAEHH95VNPvvc-S0HP_uWNyqkHZmumFZsro'
+# def edit_film(update, context):
+#     chat_id = update.message.chat_id
+#     title = context.args[0]
+#     field = context.args[1]
+#     value = ' '.join(context.args[2:])
+#     cursor.execute(f"UPDATE films SET {field}=? WHERE title=?", (value, title))
+#     conn.commit()
+#     update.message.reply_text('Информация о фильме успешно обновлена!')
 
- 
 
-# Создаем объект telegram.Bot с использованием токена
+async def help (update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    message = 'Список команд:\n\n'
+    message += '/addfilm <название> <год> <режиссер> <описание> - добавить новый фильм в вашу фильмотеку\n'
+    message += '/deletefilm <название> - удалить фильм из вашей фильмотеки\n'
+    message += '/viewfilms - просмотреть список фильмов в вашей фильмотеке\n'
+    message += '/editfilm <название> <поле> <значение> - редактировать информацию о фильме в вашей фильмотеке\n'
+    await update.message.reply_text(message)
+    
+async def view_films(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.message.chat_id
+    cursor.execute("SELECT * FROM films")
+    films = cursor.fetchall()
+    if len(films) == 0:
+        update.message.reply_text('Ваша фильмотека пуста!')
+        return
+    message = 'Список фильмов в вашей фильмотеке:\n\n'
+    for film in films:
+        message += f'{film[1]} ({film[2]})\nРежиссер: {film[3]}\nОписание: {film[4]}\n\n'
+    await update.message.reply_text(message)
 
-bot = telegram.Bot(token=TOKEN)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Добро пожаловать в вашу личную фильмотеку! Введите /help для получения списка команд.')
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Я бот для работы со справочником контактов Google.')
+async def unknown(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="ТАКИХ КОМАНДОВ НЕМА")
 
-def search(update: Update, context: CallbackContext) -> None:
-    # Получаем доступ к API Google Contacts
-    creds, _ = google.auth.default()
-    service = build('people', 'v1', credentials=creds)
 
-    # Выполняем поиск контакта
-    name = ' '.join(context.args)
-    results = service.people().connections().list(
-        resourceName='people/me',
-        personFields='names,emailAddresses',
-        q=name
-    ).execute()
 
-    # Отправляем результаты поиска пользователю
-    for person in results.get('connections', []):
-        names = person.get('names', [])
-        emails = person.get('emailAddresses', [])
-        if names and emails:
-            update.message.reply_text(f"Имя: {names[0]['displayName']}, Email: {emails[0]['value']}")
 
-def main() -> None:
-    updater = Updater(bot, use_context=True)
+app = ApplicationBuilder().token("556541562:AAEHH95VNPvvc-S0HP_uWNyqkHZmumFZsro").build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help))
+app.add_handler(CommandHandler("addfilm", add_film))
+# app.add_handler(CommandHandler("deletefilm", handle_message))
+app.add_handler(CommandHandler('viewfilms', view_films))
+# app.add_handler(CommandHandler("editfilm", handle_message))
+app.add_handler(MessageHandler(filters.COMMAND, unknown))
 
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("search", search))
-
-    updater.start_polling()
-
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+app.run_polling()
